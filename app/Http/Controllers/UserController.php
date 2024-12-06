b70d1dc0df74699bc0ebee9f63e24acab8a7f2b5
Alonso271 <alonsomadrona2719@gmail.com>
2024-12-06 21:06:06 +0100

    Arreglado el problema con las imagenes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,53 +29,55 @@ class UserController extends Controller
     
     public function update(Request $request) 
     {
-        $user     = \Auth::user();
-        GenreUser::where('user_id',$user->id)->delete();
-        Recommendation::where('user_id',$user->id)->where('viewed', '!=', 1)->delete();
+        $user = \Auth::user();
+
+        GenreUser::where('user_id', $user->id)->delete();
+        Recommendation::where('user_id', $user->id)->where('viewed', '!=', 1)->delete();
+
         $validate = $this->validate($request, [
             'name'  => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users,email,'.$user->id,
+            'email' => 'required|string|email|max:255|unique:users,email,' . $user->id,
         ]);
-        
+
         $name  = $request->input('name');
         $email = $request->input('email');
-        
+
         $user->name  = $name;
         $user->email = $email;
-        
-        if (!empty($request->genres)){
-            foreach ($request->genres as $genre => $id){
-            GenreUser::create(['user_id'=>$user->id, 'genre_id' => $id]);
+
+        if (!empty($request->genres)) {
+            foreach ($request->genres as $genre => $id) {
+                GenreUser::create(['user_id' => $user->id, 'genre_id' => $id]);
             }
 
             $genresUsers = GenreUser::where('user_id', $user->id)->get();
             $genresMovies = GenreMovie::get();
 
-            foreach($genresUsers as $genreUser){
-                foreach($genresMovies as $genreMovie){
-                if($genreUser->genre_id == $genreMovie->genre_id){
-                    Recommendation::firstOrCreate([
-                    'user_id'  => $user->id,
-                    'movie_id' => $genreMovie->movie_id,
-                    ], [
-                    'viewed'   => 0,
-                    ]);
-                }
+            foreach ($genresUsers as $genreUser) {
+                foreach ($genresMovies as $genreMovie) {
+                    if ($genreUser->genre_id == $genreMovie->genre_id) {
+                        Recommendation::firstOrCreate([
+                            'user_id'  => $user->id,
+                            'movie_id' => $genreMovie->movie_id,
+                        ], [
+                            'viewed' => 0,
+                        ]);
+                    }
                 }
             }
         }
-        
+
         $image = $request->file('image');
-        if($image){
-            $image_name = time().$image->getClientOriginalName();
-            
-            Storage::disk('users')->put($image_name, File::get($image));
-            
+        if ($image) {
+            $image_name = time() . $image->getClientOriginalName();
+
+            $image->storeAs('public/users', $image_name);
+
             $user->image = $image_name;
         }
-        
+
         $user->update();
-        
+
         return redirect()->route('config')->with(['message' => 'Usuario actualizado correctamente']);
     }
     
