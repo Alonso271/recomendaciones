3da364fc10c1841f72ca89e6304e146fa4f77c7c
Alonso271 <alonsomadrona2719@gmail.com>
2024-12-10 15:46:04 +0100

    Probando9

--- resources/views/includes/avatar.blade.php
+++ resources/views/includes/avatar.blade.php
@@ -1,6 +1,8 @@
-@if(Auth::user()->image)
-    <div class='container-avatar'>
-        <img src='{{  Auth::user()->image }}' class='avatar'>
-    </div>
+@if(!empty(Auth::user()))
+    @if(Auth::user()->image)
+        <div class='container-avatar'>
+            <img src='{{  Auth::user()->image }}' class='avatar'>
+        </div>
+    @endif
 @endif
 
